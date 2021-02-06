3f6788ba86740ab3907500cb33bdff6433daf540
Emilie Rains <emiliearains@gmail.com>
2021-02-06 09:14:11 -0500

    comments3 commit

--- SocialMedia.WebAPI/Controllers/CommentController.cs
+++ SocialMedia.WebAPI/Controllers/CommentController.cs
@@ -1,4 +1,6 @@
-﻿using SocialMedia.Services;
+﻿using Microsoft.AspNet.Identity;
+using SocialMedia.Models;
+using SocialMedia.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,13 +15,29 @@ namespace SocialMedia.WebAPI.Controllers
     [Authorize]
     public class CommentController : ApiController
     {
-        public IHttpActionResult Get()
+        public IHttpActionResult Get(int userId)
         {
             CommentService commentService = CreateCommentService();
-            var comments = commentService.GetComments();
+            var comments = commentService.GetComments(userId);
             return Ok(comments);
         }
+        public IHttpActionResult Post(CommentCreate comment)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
 
+            var service = CreateCommentService();
 
+            if (!service.CreateComment(comment))
+                return InternalServerError();
+
+            return Ok();
+        }
+        private CommentService CreateCommentService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var commentService = new CommentService(userId);
+            return commentService;
+        }
     }
 }
