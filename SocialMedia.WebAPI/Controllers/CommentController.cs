af0915dc7014cbd4a424f93ab0e640d6273131ce
Emilie Rains <emiliearains@gmail.com>
2021-02-05 23:34:35 -0500

    comment commit 2

--- SocialMedia.WebAPI/Controllers/CommentController.cs
+++ SocialMedia.WebAPI/Controllers/CommentController.cs
@@ -1,14 +1,25 @@
-﻿using System;
+﻿using SocialMedia.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
+// TO-DO: get comments by postID
+
 namespace SocialMedia.WebAPI.Controllers
 {
+    [Authorize]
     public class CommentController : ApiController
     {
-        // TO-DO: get comments by postID
+        public IHttpActionResult Get()
+        {
+            CommentService commentService = CreateCommentService();
+            var comments = commentService.GetComments();
+            return Ok(comments);
+        }
+
+
     }
 }
